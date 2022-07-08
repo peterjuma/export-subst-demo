e916dac3a9e2c96c49b9239f0975ce0912bc96d0
peterjuma <30427827+peterjuma@users.noreply.github.com>
2022-07-08 16:45:53 +0300

    removed echo and die statements, lolz.

--- sample1.py
+++ sample1.py
@@ -1,8 +1,10 @@
 
-message = '$Format:The author of %h was %an, %ar%nThe title was >>%s<<' 
+MSG = '$Format:The author of %h was %an, %ar%nThe title was >>%s<<' 
 
 COMMIT = '$Format:Last commit: %h by %aN at %cd%n%+w(76,6,9)%B'
 
-version = "$Format:%(describe:tags=true)"
+version = '$Format:%(describe:tags=true)'
+
+TEST = '$Format:%C(auto)%h %(describe:tags=true) (%s, %ad)'
 
 
