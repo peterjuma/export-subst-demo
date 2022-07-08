ca1defb4daceda57f6b22114dfb9b6e3ba4e86a5
peterjuma <30427827+peterjuma@users.noreply.github.com>
2022-07-08 16:35:39 +0300

    pep8 fixer

--- sample1.py
+++ sample1.py
@@ -1,8 +1,8 @@
 
-message = '$Format:The author of %h was %an, %ar%nThe title was >>%s<<%n' 
+message = '$Format:The author of %h was %an, %ar%nThe title was >>%s<<' 
 
-COMMIT = '$Format:Last commit: %h by %aN at %cd%n%+w(76,6,9)%B$'
+COMMIT = '$Format:Last commit: %h by %aN at %cd%n%+w(76,6,9)%B'
 
-version = "$Format:%(describe:tags=true)$"
+version = "$Format:%(describe:tags=true)"
 
 
