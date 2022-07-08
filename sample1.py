bb9a84e0489fc85d24f8c44dc1a570748b2ce49a
peterjuma <30427827+peterjuma@users.noreply.github.com>
2022-07-08 16:51:03 +0300

    We'll figure it out on Monday

--- sample1.py
+++ sample1.py
@@ -1,10 +1,9 @@
 
 MSG = '$Format:The author of %h was %an, %ar The title was >>%s<<'
 
-COMMIT = '$Format:Last commit: %h by %aN at %cd%+w(76,6,9)%B'
-
 version = '$Format:%(describe:tags=true)'
 
 TEST = '$Format:%C(auto)%h %(describe:tags=true) (%s, %ad)'
+COMMIT = '$Format:Last commit: %h by %aN at %cd%+w(76,6,9)%B'
 
 
