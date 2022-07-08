cc57c9e7b77fae0d1c114e776a261ca95c06b71c
peterjuma <30427827+peterjuma@users.noreply.github.com>
2022-07-08 17:04:51 +0300

    project lead is allergic to changes...

--- sample1.py
+++ sample1.py
@@ -5,9 +5,6 @@ MSG = '$Format:The author of %h was %an, %ar The title was >>%s<<$'
 version = '$Format:%(describe:tags=true)$'
 
 #
-TEST = '$Format:%C(auto)%h describe:tags=true (%s, %ad)$'
-
-#
 COMMIT = '$Format:Last commit: %h by %aN at %cd%+w(76,6,9)%B$'
 
 
