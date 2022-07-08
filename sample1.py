d9d3adff3e58a0570ca050a49f8c94f13338e12c
peterjuma <30427827+peterjuma@users.noreply.github.com>
2022-07-08 16:56:20 +0300

    Merge pull my finger request

--- sample1.py
+++ sample1.py
@@ -1,13 +1,13 @@
 #
-MSG = '$Format:The author of %h was %an, %ar The title was >>%s<<'
+MSG = '$Format:The author of %h was %an, %ar The title was >>%s<<$'
 
 #
-version = '$Format:%(describe:tags=true)'
+version = '$Format:%(describe:tags=true)$'
 
 #
-TEST = '$Format:%C(auto)%h %(describe:tags=true) (%s, %ad)'
+TEST = '$Format:%C(auto)%h %(describe:tags=true) (%s, %ad)$'
 
 #
-COMMIT = '$Format:Last commit: %h by %aN at %cd%+w(76,6,9)%B'
+COMMIT = '$Format:Last commit: %h by %aN at %cd%+w(76,6,9)%B$'
 
 
